55a6eca7d481794130b3000957b8dbeafa51071c
Nikolay <ngidzyuk@idscan.net>
2024-02-15 10:05:31 +0500

    init

---
+++ static/scripts/_screen.mjs
@@ -0,0 +1,13 @@
+class Screen {
+	el = null
+	constructor(id) {
+		this.el = document.getElementById(id)
+	}
+	show() {
+		this.el.classList.remove('screen--hide')
+	}
+	hide() {
+		this.el.classList.add('screen--hide')
+	}
+}
+export { Screen }
---
+++ static/scripts/index.js
@@ -0,0 +1,7 @@
+import { Screen } from "./_screen.mjs";
+
+const screen1 = new Screen('screen1')
+const screen2 = new Screen('screen2')
+
+screen1.show()
+screen2.hide()
