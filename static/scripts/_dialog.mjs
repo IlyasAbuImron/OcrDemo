03855b79c71a1e74997de16918f22ac36610383d
ihakimov <ihakimov@idscan.net>
2024-02-21 06:30:50 +0000

    Merged PR 15421: Dialog modals
    Created modals

--- static/scripts/_dialog.mjs
+++ static/scripts/_dialog.mjs
@@ -1,6 +1,77 @@
 export class Dialog {
-	constructor() {
+	constructor(dialogParams) {
+		this.modalOverlay = dialogParams.modalOverlay
+		this.loadingModal = dialogParams.loadingModal
+		this.errorModal = dialogParams.errorModal
+		this.loadingCancelButton = dialogParams.loadingCancelButton
+		this.closeErrorModalButton = dialogParams.closeErrorModalButton
+		this.errorConfirmButton = dialogParams.errorConfirmButton
+		this.addNewFileButton = dialogParams.addNewFileButton
+		this.uploadInputButton = dialogParams.uploadInputButton
+		this.loadingMessage = dialogParams.loadingMessage
+		this.errorMessage = dialogParams.errorMessage
+
+		this.loadingCancelButton.addEventListener('click', this.hideModalOverlay.bind(this))
+		this.closeErrorModalButton.addEventListener('click', this.hideModalOverlay.bind(this))
+		this.errorConfirmButton.addEventListener('click', this.hideModalOverlay.bind(this))
+
+		this.addNewFileButton.addEventListener('click', this.showUploadModal.bind(this))
+		this.addNewFileButton.addEventListener('change', (event) => {
+			if (event.target.files.length) {
+				this.showModalOverlay()
+				this.showLoadingModal()
+				this.updateLoadingMessage()
+
+				setTimeout(() => {
+					this.hideLoadingModal()
+					this.showErrorModal()
+				}, 4000)
+			}
+		})
+
 	}
 
+	showModalOverlay() {
+		this.modalOverlay.classList.add('modal--show')
+	}
 
+	showUploadModal() {
+		this.uploadInputButton.click()
+	}
+
+	onClickLoadingCancelButton(cancelRequest) {
+		this.loadingCancelButton.addEventListener('click', cancelRequest)
+	}
+
+	updateLoadingMessage() {
+		this.loadingMessage.textContent = 'Loading...'
+
+		setTimeout(() => {
+			this.loadingMessage.textContent = 'Recognition in progress...'
+		}, 2000)
+	}
+
+	showErrorMessage(message) {
+		this.errorMessage.textContent = message
+	}
+
+	hideModalOverlay() {
+		this.modalOverlay.classList.add('modal--hide')
+	}
+
+	showLoadingModal() {
+		this.loadingModal.classList.remove('modal--hide')
+	}
+
+	hideLoadingModal() {
+		this.loadingModal.classList.add('modal--hide')
+	}
+
+	showErrorModal() {
+		this.errorModal.classList.remove('modal--hide')
+	}
+
+	hideErrorModal() {
+		this.errorModal.classList.add('modal--hide')
+	}
 }
--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,4 +1,29 @@
 import { Screen } from "./_screen.mjs";
+import { Dialog } from "./_dialog.mjs";
+
+const dialogParams = {
+    modalOverlay: document.getElementById('modal-overlay'),
+    loadingModal: document.getElementById('loading-modal'),
+    errorModal: document.getElementById('error-modal'),
+    loadingCancelButton: document.getElementById('loading-cancel-button'),
+    closeErrorModalButton: document.getElementById('close-error-modal-button'),
+    errorConfirmButton: document.getElementById('error-confirm-button'),
+    addNewFileButton: document.getElementById('add-new-file'),
+    uploadInputButton: document.getElementById('upload-input'),
+    loadingMessage: document.querySelector('.loading-message'),
+    errorMessage: document.querySelector('.error-message')
+}
+
+const modal = new Dialog(dialogParams)
+
+const cancelRequest = () => {
+    console.log('Cancel request')
+}
+modal.onClickLoadingCancelButton(cancelRequest)
+
+modal.showErrorMessage('File is corrupted')
+
+modal.hideErrorModal()
 
 const screen1 = new Screen('screen1')
 const screen2 = new Screen('screen2')
